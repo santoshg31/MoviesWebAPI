f330ce2228b359ebfded348cd312bfc45ca5eee7
Santosh Gawde <s.gawde@nfer.ac.uk>
2021-01-10 19:28:20 +0000

    added Automapper to the project

---
+++ MoviesWebAPI/AutoMapperProfiles/MoviesProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MoviesWebAPI.Core.Models;
+using MoviesWebAPI.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesWebAPI.AutoMapperProfiles
+{
+    public class MoviesProfile:Profile
+    {
+        public MoviesProfile()
+        {
+            CreateMap<Movie, MovieDto>().ReverseMap();
+        }
+
+    }
+}
